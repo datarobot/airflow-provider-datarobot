e2422a11dbfa957f6b7ef6c76f0739b39cc73cb6
Oleksandr Saienko <oleksandr.saienko@datarobot.com>
2023-04-14 15:41:12 +0100

    fixed make cmd

--- jenkins/style_check.groovy
+++ jenkins/style_check.groovy
@@ -7,7 +7,7 @@ stage('Style Check') {
               virtualenv .venv -p python3.8
               source .venv/bin/activate
               pip install -r requirements.txt
-              make  make lint fix-licenses check-licenses
+              make lint fix-licenses check-licenses
             """
     }
 }
