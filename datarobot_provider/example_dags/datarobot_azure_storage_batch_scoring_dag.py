e605d466647b3a0489e0428fd70c9b30ea17cbc6
Oleksandr Saienko <49866430+oleksandr-saienko@users.noreply.github.com>
2023-05-18 15:37:19 +0100

    Update datarobot_provider/example_dags/datarobot_azure_storage_batch_scoring_dag.py
    Co-authored-by: Erik Lattimore <elatt@users.noreply.github.com>

--- datarobot_provider/example_dags/datarobot_azure_storage_batch_scoring_dag.py
+++ datarobot_provider/example_dags/datarobot_azure_storage_batch_scoring_dag.py
@@ -55,7 +55,7 @@ from datarobot_provider.sensors.datarobot import ScoringCompleteSensor
         },
     },
 )
-def datarobot_azure_storage_batch_scoring(deployment_id="put_your_deployment_id"):
+def datarobot_azure_storage_batch_scoring(deployment_id=None):
     if not deployment_id:
         raise ValueError("Invalid or missing `deployment_id` value")
 
