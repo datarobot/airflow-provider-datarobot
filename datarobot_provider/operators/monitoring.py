3e7da1027a117c2b1650136d859b291c91bb5e77
Oleksandr Saienko <49866430+oleksandr-saienko@users.noreply.github.com>
2023-06-01 19:39:40 +0100

    [AGENT-4519] Implement Operator for Reading Deployment Accuracy metrics (#34)

--- datarobot_provider/operators/monitoring.py
+++ datarobot_provider/operators/monitoring.py
@@ -60,13 +60,57 @@ class GetServiceStatsOperator(BaseOperator):
         deployment = dr.Deployment.get(self.deployment_id)
         service_stats_params = context["params"].get("service_stats", {})
         service_stats = deployment.get_service_stats(**service_stats_params)
-        return _serialize_service_stats(service_stats)
+        return _serialize_metrics(service_stats)
 
 
-def _serialize_service_stats(service_stats_obj, date_format=DATETIME_FORMAT):
+def _serialize_metrics(service_stats_obj, date_format=DATETIME_FORMAT):
     service_stats_dict = service_stats_obj.__dict__.copy()
     service_stats_dict["period"] = {
         "start": service_stats_obj.period["start"].strftime(date_format),
         "end": service_stats_obj.period["end"].strftime(date_format),
     }
     return service_stats_dict
+
+
+class GetAccuracyOperator(BaseOperator):
+    """
+    Gets the accuracy of a deployment’s predictions.
+
+    :param deployment_id: DataRobot deployment ID
+    :type deployment_id: str
+    :param datarobot_conn_id: Connection ID, defaults to `datarobot_default`
+    :type datarobot_conn_id: str, optional
+    :return: Service stats for a Deployment
+    :rtype: List[dict]
+    """
+
+    # Specify the arguments that are allowed to parse with jinja templating
+    template_fields: Iterable[str] = ["deployment_id"]
+    template_fields_renderers: Dict[str, str] = {}
+    template_ext: Iterable[str] = ()
+    ui_color = "#f4a460"
+
+    def __init__(
+        self,
+        *,
+        deployment_id: str,
+        datarobot_conn_id: str = "datarobot_default",
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(**kwargs)
+        self.deployment_id = deployment_id
+        self.datarobot_conn_id = datarobot_conn_id
+        if kwargs.get("xcom_push") is not None:
+            raise AirflowException(
+                "'xcom_push' was deprecated, use 'BaseOperator.do_xcom_push' instead"
+            )
+
+    def execute(self, context: Dict[str, Any]) -> List[dict]:
+        # Initialize DataRobot client
+        DataRobotHook(datarobot_conn_id=self.datarobot_conn_id).run()
+
+        self.log.info(f"Getting service stats for deployment_id={self.deployment_id}")
+        deployment = dr.Deployment.get(self.deployment_id)
+        accuracy_params = context["params"].get("accuracy", {})
+        accuracy = deployment.get_accuracy(**accuracy_params)
+        return _serialize_metrics(accuracy)
--- tests/unit/operators/test_monitoring.py
+++ tests/unit/operators/test_monitoring.py
@@ -9,10 +9,12 @@ from datetime import datetime
 
 import datarobot as dr
 import pytest
+from datarobot.models import Accuracy
 from datarobot.models import ServiceStats
 
+from datarobot_provider.operators.monitoring import GetAccuracyOperator
 from datarobot_provider.operators.monitoring import GetServiceStatsOperator
-from datarobot_provider.operators.monitoring import _serialize_service_stats
+from datarobot_provider.operators.monitoring import _serialize_metrics
 
 
 @pytest.fixture
@@ -42,7 +44,7 @@ def service_stat_details():
 def test_operator_get_service_stat(mocker, service_stat_details):
     deployment_id = "deployment-id"
     service_stat = ServiceStats(**service_stat_details)
-    expected_service_stats = _serialize_service_stats(service_stat)
+    expected_service_stats = _serialize_metrics(service_stat)
 
     mocker.patch.object(dr.Deployment, "get", return_value=dr.Deployment(deployment_id))
     get_service_stats_mock = mocker.patch.object(
@@ -60,7 +62,7 @@ def test_operator_get_service_stat(mocker, service_stat_details):
 def test_operator_get_service_stat_with_params(mocker, service_stat_details):
     deployment_id = "deployment-id"
     service_stat = ServiceStats(**service_stat_details)
-    expected_service_stats = _serialize_service_stats(service_stat)
+    expected_service_stats = _serialize_metrics(service_stat)
 
     mocker.patch.object(dr.Deployment, "get", return_value=dr.Deployment(deployment_id))
     get_service_stats_mock = mocker.patch.object(
@@ -81,3 +83,81 @@ def test_operator_get_service_stat_with_params(mocker, service_stat_details):
 
     assert service_stats_result == expected_service_stats
     get_service_stats_mock.assert_called_with(**service_stats_params["service_stats"])
+
+
+@pytest.fixture
+def accuracy_details():
+    return {
+        'model_id': "test-model-id",
+        'period': {
+            'start': datetime.fromisoformat("2023-05-01"),
+            'end': datetime.fromisoformat("2023-05-07"),
+        },
+        'metrics': {
+            'RMSE': {
+                'value': 212.951709001837,
+                'baseline_value': 1968.4643697912,
+                'percent_change': 89.18,
+            },
+            'MAE': {
+                'value': 212.511998013901,
+                'baseline_value': 1366.90728550895,
+                'percent_change': 84.45,
+            },
+            'Gamma Deviance': {
+                'value': 0.000417428054515133,
+                'baseline_value': 0.0131687235807189,
+                'percent_change': 96.83,
+            },
+            'Tweedie Deviance': {
+                'value': 0.0425429308927789,
+                'baseline_value': 1.57434163465787,
+                'percent_change': 97.3,
+            },
+            'R Squared': {
+                'value': 0.8695494587538976,
+                'baseline_value': 0.9366663850412537,
+                'percent_change': -7.17,
+            },
+        },
+    }
+
+
+def test_operator_get_accuracy(mocker, accuracy_details):
+    deployment_id = "deployment-id"
+    accuracy = Accuracy(**accuracy_details)
+    expected_accuracy = _serialize_metrics(accuracy)
+
+    mocker.patch.object(dr.Deployment, "get", return_value=dr.Deployment(deployment_id))
+    get_accuracy_mock = mocker.patch.object(dr.Deployment, "get_accuracy", return_value=accuracy)
+
+    operator = GetAccuracyOperator(task_id="get_accuracy", deployment_id="deployment-id")
+    accuracy_params = {}
+    accuracy_result = operator.execute(context={'params': {}})
+
+    assert accuracy_result == expected_accuracy
+    get_accuracy_mock.assert_called_with(**accuracy_params)
+
+
+def test_operator_get_accuracy_with_params(mocker, accuracy_details):
+    deployment_id = "deployment-id"
+    accuracy = Accuracy(**accuracy_details)
+    expected_accuracy = _serialize_metrics(accuracy)
+
+    mocker.patch.object(dr.Deployment, "get", return_value=dr.Deployment(deployment_id))
+    get_accuracy_mock = mocker.patch.object(dr.Deployment, "get_accuracy", return_value=accuracy)
+
+    operator = GetAccuracyOperator(task_id="get_accuracy", deployment_id="deployment-id")
+
+    accuracy_params = {
+        "accuracy": {
+            "model_id": "test-model-id",
+            "start_time": datetime.fromisoformat("2023-05-01"),
+            "end_time": datetime.fromisoformat("2023-05-07"),
+        }
+    }
+
+    accuracy_result = operator.execute(context={'params': accuracy_params})
+
+    assert accuracy_result == expected_accuracy
+    get_accuracy_mock.assert_called_with(**accuracy_params["accuracy"])
