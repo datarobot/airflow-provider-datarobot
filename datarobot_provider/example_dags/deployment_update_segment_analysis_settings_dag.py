499af48044bedc8dd6d47e6af9bde8f4f366b46a
Oleksandr Saienko <oleksandr.saienko@datarobot.com>
2023-06-22 18:17:43 +0100

    added operators for Segment Analysis and Bias&Fairness

---
+++ datarobot_provider/example_dags/deployment_update_segment_analysis_settings_dag.py
@@ -0,0 +1,122 @@
+# Copyright 2023 DataRobot, Inc. and its affiliates.
+#
+# All rights reserved.
+#
+# This is proprietary source code of DataRobot, Inc. and its affiliates.
+#
+# Released under the terms of DataRobot Tool and Utility Agreement.
+from datetime import datetime
+
+from airflow.decorators import dag
+from airflow.decorators import task
+
+from datarobot_provider.operators.bias_and_fairness import (
+    GetBiasAndFairnessSettingsOperator,
+    UpdateBiasAndFairnessSettingsOperator,
+)
+from datarobot_provider.operators.segment_analysis import (
+    GetSegmentAnalysisSettingsOperator,
+    UpdateSegmentAnalysisSettingsOperator,
+)
+
+
+@dag(
+    schedule=None,
+    start_date=datetime(2023, 1, 1),
+    tags=['example', 'mlops'],
+    # Default json config example:
+    params={
+        'segment_analysis_enabled': True,
+        'segment_analysis_attributes': ['race', 'gender'],
+        'protected_features': ['gender'],
+        'preferable_target_value': 'True',
+        'fairness_metrics_set': 'equalParity',
+        'fairness_threshold': 0.1
+    },
+)
+def deployment_segment_analysis_settings(deployment_id='641db050023431666af91a90'):
+    if not deployment_id:
+        raise ValueError("Invalid or missing `deployment_id` value")
+    get_segment_analysis_settings_before_op = GetSegmentAnalysisSettingsOperator(
+        task_id="get_segment_analysis_settings_before",
+        # you can pass deployment_id from previous operator here:
+        deployment_id=deployment_id,
+    )
+
+    get_bias_and_fairness_settings_before_op = GetBiasAndFairnessSettingsOperator(
+        task_id="get_bias_and_fairness_settings_before",
+        # you can pass deployment_id from previous operator here:
+        deployment_id=deployment_id,
+    )
+
+    update_segment_analysis_settings_op = UpdateSegmentAnalysisSettingsOperator(
+        task_id="update_segment_analysis_settings",
+        # you can pass deployment_id from previous operator here:
+        deployment_id=deployment_id,
+    )
+
+    update_bias_and_fairness_settings_op = UpdateBiasAndFairnessSettingsOperator(
+        task_id="update_bias_and_fairness_settings",
+        # you can pass deployment_id from previous operator here:
+        deployment_id=deployment_id,
+    )
+
+    get_segment_analysis_settings_after_op = GetSegmentAnalysisSettingsOperator(
+        task_id="get_segment_analysis_after",
+        # you can pass deployment_id from previous operator here:
+        deployment_id=deployment_id,
+    )
+
+    get_bias_and_fairness_settings_after_op = GetBiasAndFairnessSettingsOperator(
+        task_id="get_bias_and_fairness_settings_after",
+        # you can pass deployment_id from previous operator here:
+        deployment_id=deployment_id,
+    )
+
+    @task(task_id="example_processing_python")
+    def settings_processing(
+        segment_analysis_settings_before,
+        segment_analysis_settings_after,
+        bias_and_fairness_settings_before,
+        bias_and_fairness_settings_after,
+    ):
+        """Example of custom logic based on segment analysis settings from the deployment."""
+
+        # Put your service stat processing logic here:
+
+        segment_analysis_settings_changed = {
+            setting: segment_analysis_settings_after[setting]
+            for setting in segment_analysis_settings_after
+            if segment_analysis_settings_after[setting] != segment_analysis_settings_before[setting]
+        }
+
+        bias_and_fairness_settings_changed = {
+            setting: bias_and_fairness_settings_after[setting]
+            for setting in bias_and_fairness_settings_after
+            if bias_and_fairness_settings_after[setting] != bias_and_fairness_settings_before[setting]
+        }
+
+        return segment_analysis_settings_changed, bias_and_fairness_settings_changed
+
+    example_service_stat_processing = settings_processing(
+        segment_analysis_settings_before=get_segment_analysis_settings_before_op.output,
+        segment_analysis_settings_after=get_segment_analysis_settings_after_op.output,
+        bias_and_fairness_settings_before=get_bias_and_fairness_settings_before_op.output,
+        bias_and_fairness_settings_after=get_bias_and_fairness_settings_after_op.output,
+    )
+
+    (
+        get_segment_analysis_settings_before_op
+        >> get_bias_and_fairness_settings_before_op
+        >> update_segment_analysis_settings_op
+        >> update_bias_and_fairness_settings_op
+        >> get_segment_analysis_settings_after_op
+        >> get_bias_and_fairness_settings_after_op
+        >> example_service_stat_processing
+    )
+
+
+deployment_segment_analysis_dag = deployment_segment_analysis_settings()
+
+if __name__ == "__main__":
+    deployment_segment_analysis_dag.test()
---
+++ datarobot_provider/operators/segment_analysis.py
@@ -0,0 +1,125 @@
+# Copyright 2023 DataRobot, Inc. and its affiliates.
+#
+# All rights reserved.
+#
+# This is proprietary source code of DataRobot, Inc. and its affiliates.
+#
+# Released under the terms of DataRobot Tool and Utility Agreement.
+from typing import Any
+from typing import Dict
+from typing import Iterable
+
+import datarobot as dr
+from airflow.exceptions import AirflowException
+from airflow.models import BaseOperator
+
+from datarobot_provider.hooks.datarobot import DataRobotHook
+
+
+class GetSegmentAnalysisSettingsOperator(BaseOperator):
+    """
+    Get segment analysis settings for deployment.
+
+    :param deployment_id: DataRobot deployment ID
+    :type deployment_id: str
+    :param datarobot_conn_id: Connection ID, defaults to `datarobot_default`
+    :type datarobot_conn_id: str, optional
+    """
+
+    # Specify the arguments that are allowed to parse with jinja templating
+    template_fields: Iterable[str] = ["deployment_id"]
+    template_fields_renderers: Dict[str, str] = {}
+    template_ext: Iterable[str] = ()
+    ui_color = "#f4a460"
+
+    def __init__(
+        self,
+        *,
+        deployment_id: str,
+        datarobot_conn_id: str = "datarobot_default",
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(**kwargs)
+        self.deployment_id = deployment_id
+        self.datarobot_conn_id = datarobot_conn_id
+        if kwargs.get("xcom_push") is not None:
+            raise AirflowException(
+                "'xcom_push' was deprecated, use 'BaseOperator.do_xcom_push' instead"
+            )
+
+    def execute(self, context: Dict[str, Any]) -> dict:
+        # Initialize DataRobot client
+        DataRobotHook(datarobot_conn_id=self.datarobot_conn_id).run()
+
+        self.log.info(f"Get Deployment for deployment_id={self.deployment_id}")
+        deployment = dr.Deployment.get(deployment_id=self.deployment_id)
+
+        segment_analysis_settings = deployment.get_segment_analysis_settings()
+
+        return segment_analysis_settings
+
+
+class UpdateSegmentAnalysisSettingsOperator(BaseOperator):
+    """
+    Update segment analysis settings for deployment.
+
+    :param deployment_id: DataRobot deployment ID
+    :type deployment_id: str
+    :param datarobot_conn_id: Connection ID, defaults to `datarobot_default`
+    :type datarobot_conn_id: str, optional
+    """
+
+    # Specify the arguments that are allowed to parse with jinja templating
+    template_fields: Iterable[str] = ["deployment_id"]
+    template_fields_renderers: Dict[str, str] = {}
+    template_ext: Iterable[str] = ()
+    ui_color = "#f4a460"
+
+    def __init__(
+        self,
+        *,
+        deployment_id: str,
+        datarobot_conn_id: str = "datarobot_default",
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(**kwargs)
+        self.deployment_id = deployment_id
+        self.datarobot_conn_id = datarobot_conn_id
+        if kwargs.get("xcom_push") is not None:
+            raise AirflowException(
+                "'xcom_push' was deprecated, use 'BaseOperator.do_xcom_push' instead"
+            )
+
+    def execute(self, context: Dict[str, Any]) -> None:
+        # Initialize DataRobot client
+        DataRobotHook(datarobot_conn_id=self.datarobot_conn_id).run()
+
+        self.log.info(f"Getting Deployment for deployment_id={self.deployment_id}")
+        deployment = dr.Deployment.get(deployment_id=self.deployment_id)
+
+        current_segment_analysis_settings = deployment.get_segment_analysis_settings()
+
+        segment_analysis_enabled = context["params"].get(
+            "segment_analysis_enabled", current_segment_analysis_settings['enabled']
+        )
+        segment_analysis_attributes = context["params"].get(
+            "segment_analysis_attributes", current_segment_analysis_settings['attributes']
+        )
+
+        if (segment_analysis_enabled != current_segment_analysis_settings['enabled']) or (
+            segment_analysis_attributes != current_segment_analysis_settings['attributes']
+        ):
+            self.log.debug(
+                f"Trying to update segment analysis settings for deployment_id={self.deployment_id}"
+            )
+            deployment.update_segment_analysis_settings(
+                segment_analysis_enabled=segment_analysis_enabled,
+                segment_analysis_attributes=segment_analysis_attributes,
+            )
+            self.log.info(
+                f"Deployment segment analysis settings updated for deployment_id={self.deployment_id}"
+            )
+        else:
+            self.log.info(
+                f"No need to update segment analysis settings for deployment_id={self.deployment_id}"
+            )
