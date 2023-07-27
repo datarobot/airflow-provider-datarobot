56868e3818214dbc6c4e44ad4a00c967256e9a86
Oleksandr Saienko <49866430+oleksandr-saienko@users.noreply.github.com>
2023-07-27 10:23:04 +0100

    [AGENT-4766] Implement Airflow Operator to request model predictions on external dataset (#51)

--- tests/unit/operators/test_model_predictions.py
+++ tests/unit/operators/test_model_predictions.py
@@ -9,6 +9,7 @@
 import datarobot as dr
 
 from datarobot_provider.operators.model_predictions import AddExternalDatasetOperator
+from datarobot_provider.operators.model_predictions import RequestModelPredictionsOperator
 
 
 def test_operator_add_external_dataset(mocker):
@@ -48,3 +49,36 @@ def test_operator_add_external_dataset(mocker):
         max_wait=600,
     )
     assert result == external_dataset_id
+
+
+def test_operator_request_model_predictions(mocker):
+    project_id = "test-project-id"
+    model_id = "test-model-id"
+    external_dataset_id = "test-external-dataset-id"
+    predictions_job_id = "test-predictions-job-id"
+
+    model_mock = mocker.Mock()
+    model_mock.id = model_id
+    model_mock.project_id = project_id
+
+    get_model_mock = mocker.patch.object(dr.models.Model, "get", return_value=model_mock)
+
+    predictions_job_mock = mocker.Mock()
+    predictions_job_mock.id = predictions_job_id
+
+    request_predictions_mock = mocker.patch.object(
+        model_mock, "request_predictions", return_value=predictions_job_mock
+    )
+
+    operator = RequestModelPredictionsOperator(
+        task_id="request_model_predictions",
+        project_id=project_id,
+        model_id=model_id,
+        external_dataset_id=external_dataset_id,
+    )
+
+    result = operator.execute(context={"params": {}})
+
+    get_model_mock.assert_called_with(project_id, model_id)
+    request_predictions_mock.assert_called_with(dataset_id=external_dataset_id)
+    assert result == predictions_job_id
