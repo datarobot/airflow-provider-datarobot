0225f10372edd12524a4d93564b94fec34b18a7a
Oleksandr Saienko <49866430+oleksandr-saienko@users.noreply.github.com>
2023-08-14 11:41:11 +0100

    [AGENT-4685] Implement Airflow Operator to Kickoff SHAP (#57)

--- datarobot_provider/operators/model_insights.py
+++ datarobot_provider/operators/model_insights.py
@@ -130,3 +130,59 @@ class ComputeFeatureEffectsOperator(BaseOperator):
         self.log.info(f"Feature Effects Job submitted, job_id={job.id}")
 
         return job.id
+
+
+class ComputeShapOperator(BaseOperator):
+    """
+    Creates SHAP impact job in DataRobot.
+    :param project_id: DataRobot project ID
+    :type project_id: str
+    :param model_id: DataRobot model ID
+    :type model_id: str
+    :param datarobot_conn_id: Connection ID, defaults to `datarobot_default`
+    :type datarobot_conn_id: str, optional
+    :return: SHAP impact job ID
+    :rtype: str
+    """
+
+    # Specify the arguments that are allowed to parse with jinja templating
+    template_fields: Iterable[str] = [
+        "project_id",
+        "model_id",
+    ]
+    template_fields_renderers: Dict[str, str] = {}
+    template_ext: Iterable[str] = ()
+    ui_color = '#f4a460'
+
+    def __init__(
+        self,
+        *,
+        project_id: str = None,
+        model_id: str = None,
+        datarobot_conn_id: str = "datarobot_default",
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(**kwargs)
+        self.project_id = project_id
+        self.model_id = model_id
+        self.datarobot_conn_id = datarobot_conn_id
+        if kwargs.get('xcom_push') is not None:
+            raise AirflowException(
+                "'xcom_push' was deprecated, use 'BaseOperator.do_xcom_push' instead"
+            )
+
+    def execute(self, context: Dict[str, Any]) -> str:
+        # Initialize DataRobot client
+        DataRobotHook(datarobot_conn_id=self.datarobot_conn_id).run()
+
+        if self.project_id is None:
+            raise ValueError("project_id is required to compute SHAP impact.")
+
+        if self.model_id is None:
+            raise ValueError("model_id is required to compute SHAP impact.")
+
+        shap_impact_job = dr.ShapImpact.create(project_id=self.project_id, model_id=self.model_id)
+
+        self.log.info(f"Compute SHAP impact Job submitted, job_id={shap_impact_job.id}")
+
+        return shap_impact_job.id
