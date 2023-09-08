3bbaa1b356c870f625a8b14c5f9c7536c16ec376
Oleksandr Saienko <oleksandr.saienko@datarobot.com>
2023-09-08 15:59:13 +0100

    fixed unit-tests

--- jenkins/unit_test.groovy
+++ jenkins/unit_test.groovy
@@ -9,6 +9,7 @@ stage('Unit Test') {
               source .venv/bin/activate
               pip install apache-airflow==2.5.1
               pip install -r requirements.txt
+              pip install airflow-provider-datarobot
               airflow db init
               airflow db check
               pytest -vv tests/unit/ --junit-xml=unit_test_report.xml
