b1ba2862c2494cabd12dc88f0c6ab849171db9a5
Anatolii Stehnii <anatolii.stehnii@datarobot.com>
2025-01-20 14:26:15 +0200

    Add test for BC

--- tests/unit/operators/test_bias_and_fairness.py
+++ tests/unit/operators/test_bias_and_fairness.py
@@ -79,6 +79,44 @@ def test_operator_update_bias_and_fairness_settings(mocker, bias_and_fairness_se
     )
 
 
+def test_operator_update_bias_and_fairness_settings__backward_compatibility(
+    mocker, bias_and_fairness_settings_details
+):
+    deployment_id = "deployment-id"
+
+    bias_and_fairness_settings_params = {
+        "protected_features": ["gender"],
+        "preferable_target_value": "True",
+        "fairness_metrics_set": "equalParity",
+        "fairness_threshold": 0.25,
+    }
+
+    mocker.patch.object(dr.Deployment, "get", return_value=dr.Deployment(deployment_id))
+
+    mocker.patch.object(
+        dr.Deployment,
+        "get_bias_and_fairness_settings",
+        return_value=bias_and_fairness_settings_details,
+    )
+
+    update_bias_and_fairness_settings_mock = mocker.patch.object(
+        dr.Deployment, "update_bias_and_fairness_settings"
+    )
+
+    operator = UpdateBiasAndFairnessSettingsOperator(
+        task_id="update_bias_and_fairness_settings", deployment_id="deployment-id"
+    )
+
+    operator.execute(context={"params": bias_and_fairness_settings_params})
+
+    update_bias_and_fairness_settings_mock.assert_called_with(
+        protected_features=bias_and_fairness_settings_params["protected_features"],
+        fairness_metric_set=bias_and_fairness_settings_params["fairness_metrics_set"],
+        fairness_threshold=bias_and_fairness_settings_params["fairness_threshold"],
+        preferable_target_value=bias_and_fairness_settings_params["preferable_target_value"],
+    )
+
+
 def test_operator_no_need_update_bias_and_fairness_settings(
     mocker, bias_and_fairness_settings_details
 ):
