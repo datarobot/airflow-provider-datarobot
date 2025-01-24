b36c698ce6e6795dea488b5608d780e0a1673723
Matt Nitzken <mjnitz02@gmail.com>
2025-01-24 10:32:06 -0500

    [MLDEV-1178] Move existing dags to deprecated (#117)

--- tests/unit/dags/predictions/test_datarobot_s3_batch_scoring_dag.py
+++ tests/unit/dags/predictions/test_datarobot_s3_batch_scoring_dag.py
@@ -7,7 +7,7 @@
 # Released under the terms of DataRobot Tool and Utility Agreement.
 import pytest
 
-from datarobot_provider.example_dags.datarobot_aws_s3_batch_scoring_dag import (
+from datarobot_provider.example_dags.deprecated.datarobot_aws_s3_batch_scoring_dag import (
     datarobot_s3_batch_scoring,
 )
 
