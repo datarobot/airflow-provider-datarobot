70781fc755bb80b30896dd065b609fe8f3e6993c
Pavlo Sliusar <pavlo.sliusar@datarobot.com>
2025-02-05 12:36:04 +0100

    [MMM-18320]: refactor code for operator

--- datarobot_provider/operators/model_registry.py
+++ datarobot_provider/operators/model_registry.py
@@ -1,8 +1,20 @@
-from typing import Any, Dict, Optional, Sequence
-from airflow.models import BaseOperator
-from airflow.exceptions import AirflowException
-from airflow.utils.context import Context
+from enum import Enum
+from typing import Any
+from typing import Dict
+from typing import Sequence
+
 import datarobot as dr
+from airflow.exceptions import AirflowException
+from airflow.models import BaseOperator
+from airflow.utils.context import Context
+
+from datarobot_provider.hooks.datarobot import DataRobotHook
+
+
+class ModelType(Enum):
+    LEADERBOARD = "leaderboard"
+    CUSTOM = "custom"
+    EXTERNAL = "external"
 
 
 class CreateRegisteredModelVersionOperator(BaseOperator):
@@ -22,11 +34,11 @@ class CreateRegisteredModelVersionOperator(BaseOperator):
     ui_color = "#f4a460"
 
     def __init__(
-            self,
-            *,
-            model_version_params: Dict[str,  Any],
-            datarobot_conn_id: str = "datarobot_default",
-            **kwargs: Any,
+        self,
+        *,
+        model_version_params: Dict[str, Any],
+        datarobot_conn_id: str = "datarobot_default",
+        **kwargs: Any,
     ) -> None:
         super().__init__(**kwargs)
         self.model_version_params = model_version_params
@@ -34,36 +46,52 @@ class CreateRegisteredModelVersionOperator(BaseOperator):
 
     def execute(self, context: Context) -> str:
         """Executes the operator to create a registered model version."""
+        DataRobotHook(datarobot_conn_id=self.datarobot_conn_id).run()
+
         model_type = self.model_version_params.get("model_type")
         model_name = self.model_version_params.get("name")
 
         if not model_type:
             raise ValueError("'model_type' must be specified in model_version_params.")
 
-        if model_type == "leaderboard":
-            model_id = self.model_version_params["model_id"]
-            registered_model_name = self.model_version_params["registered_model_name"]
+        try:
+            model_type_enum = ModelType(model_type)
+        except ValueError:
+            raise AirflowException(f"Invalid model_type: {model_type}") from None
 
-            version = dr.RegisteredModelVersion.create_for_leaderboard_item(
-                model_id=model_id, name=model_name, registered_model_name=registered_model_name
-            )
+        model_creation_methods = {
+            ModelType.LEADERBOARD: self.create_for_leaderboard,
+            ModelType.CUSTOM: self.create_for_custom,
+            ModelType.EXTERNAL: self.create_for_external,
+        }
 
-        elif model_type == "custom":
-            custom_model_version_id = self.model_version_params["custom_model_version_id"]
+        create_method = model_creation_methods.get(model_type_enum)
+        if not create_method:
+            raise AirflowException(f"Unsupported model_type: {model_type}")
 
-            version = dr.RegisteredModelVersion.create_for_custom_model_version(
-                custom_model_version_id=custom_model_version_id, name=model_name
-            )
-
-        elif model_type == "external":
-            registered_model_id = self.model_version_params["registered_model_id"]
-            target = self.model_version_params["target"]
-
-            version = dr.RegisteredModelVersion.create_for_external(
-                name=model_name, target=target, registered_model_id=registered_model_id
-            )
-        else:
-            raise AirflowException(f"Invalid model_type: {model_type}")
-
-        self.log.info(f"Registered Model Version Created: {version.id}")
+        version = create_method(model_name)
+        self.log.info(f"Successfully created model version: {version.id}")
         return version.id
+
+    def create_for_leaderboard(self, model_name):
+        """Creates a registered model version from a leaderboard model."""
+        return dr.RegisteredModelVersion.create_for_leaderboard_item(
+            model_id=self.model_version_params["model_id"],
+            name=model_name,
+            registered_model_name=self.model_version_params["registered_model_name"],
+        )
+
+    def create_for_custom(self, model_name):
+        """Creates a registered model version for a custom model."""
+        return dr.RegisteredModelVersion.create_for_custom_model_version(
+            custom_model_version_id=self.model_version_params["custom_model_version_id"],
+            name=model_name,
+        )
+
+    def create_for_external(self, model_name):
+        """Creates a registered model version for an external model."""
+        return dr.RegisteredModelVersion.create_for_external(
+            name=model_name,
+            target=self.model_version_params["target"],
+            registered_model_id=self.model_version_params["registered_model_id"],
+        )
