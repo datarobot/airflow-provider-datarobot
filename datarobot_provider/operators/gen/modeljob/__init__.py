fa280ab4690975274c26bcc37016109ebe4ba7ce
Christopher Russell-Walker <christopher.russell-walker@datarobot.com>
2025-02-06 18:40:59 -0500

    [-] Add missing copyright headers

--- datarobot_provider/operators/gen/modeljob/__init__.py
+++ datarobot_provider/operators/gen/modeljob/__init__.py
@@ -1,1 +1,8 @@
+# Copyright 2025 DataRobot, Inc. and its affiliates.
+#
+# All rights reserved.
+#
+# This is proprietary source code of DataRobot, Inc. and its affiliates.
+#
+# Released under the terms of DataRobot Tool and Utility Agreement.
 from .model_job_get_operator import ModelJobGetOperator as ModelJobGetOperator
