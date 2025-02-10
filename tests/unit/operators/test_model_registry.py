4469ebca82e10911228a476401bf51a7fd4af136
Pavlo Sliusar <pavlo.sliusar@datarobot.com>
2025-02-10 14:05:47 +0100

    [MMM-18320]: add copyright to model registry tests

--- tests/unit/operators/test_model_registry.py
+++ tests/unit/operators/test_model_registry.py
@@ -1,3 +1,10 @@
+# Copyright 2025 DataRobot, Inc. and its affiliates.
+#
+# All rights reserved.
+#
+# This is proprietary source code of DataRobot, Inc. and its affiliates.
+#
+# Released under the terms of DataRobot Tool and Utility Agreement.
 import datarobot as dr
 
 from datarobot_provider.hooks.datarobot import DataRobotHook
