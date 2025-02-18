a30966fd227445c8f0ac202e51f8e43c18abdd91
Justin McCarthy <justin.mccarthy@datarobot.com>
2025-02-18 08:51:23 +0800

    updated with dzmitrys comments

--- datarobot_provider/example_dags/feature_discovery_example.py
+++ datarobot_provider/example_dags/feature_discovery_example.py
@@ -7,7 +7,6 @@
 # Released under the terms of DataRobot Tool and Utility Agreement.
 
 from airflow.decorators import dag
-from airflow.models.baseoperator import cross_downstream
 from datarobot import AUTOPILOT_MODE
 
 from datarobot_provider.operators.ai_catalog import UploadDatasetOperator
@@ -15,7 +14,6 @@ from datarobot_provider.operators.autopilot import StartAutopilotOperator
 from datarobot_provider.operators.datarobot import CreateProjectOperator
 from datarobot_provider.operators.datarobot import GetOrCreateUseCaseOperator
 from datarobot_provider.operators.feature_discovery import CreateFeatureDiscoveryRecipeOperator
-from datarobot_provider.operators.feature_discovery import DatasetRelationshipOperator
 from datarobot_provider.sensors.datarobot import AutopilotCompleteSensor
 
 
@@ -72,61 +70,28 @@ def datarobot_feature_discovery_pipeline(
         "catalogId": upload_transactions_dataset.output,
     }
 
-    # Alternatively, you can define a DatasetDefinitionOperator:
-    # transaction_dataset_definition = DatasetDefinitionOperator(
-    #     task_id="transaction_dataset_definition",
-    #     dataset_identifier="transactions",
-    #     dataset_id="<Dataset  ID>",
-    #     dataset_version_id="<Dataset Version ID>",
-    # ).output
-
     # Define the relationships between the datasets.
     # You do not need to specify dataset1Identifier when joining with the primary dataset.
-    primary_profile_relationship = DatasetRelationshipOperator(
-        task_id="primary_profile_relationship",
-        dataset2_identifier="profile",  # to profile
-        dataset1_keys=["CustomerID"],  # on CustomerID
-        dataset2_keys=["CustomerID"],
-    )
+    primary_profile_relationship = {
+        "dataset2Identifier": "profile",  # to profile
+        "dataset1Keys": ["CustomerID"],  # on CustomerID
+        "dataset2Keys": ["CustomerID"],
+    }
 
-    profile_transaction_relationship = DatasetRelationshipOperator(
-        task_id="profile_transaction_relationship",
-        dataset1_identifier="profile",  # join profile
-        dataset2_identifier="transactions",  # to transactions
-        dataset1_keys=["CustomerID"],  # on CustomerID
-        dataset2_keys=["CustomerID"],
-    )
+    profile_transaction_relationship = {
+        "dataset1Identifier": "profile",  # join profile
+        "dataset2Identifier": "transactions",  # to transactions
+        "dataset1Keys": ["CustomerID"],  # on CustomerID
+        "dataset2Keys": ["CustomerID"],
+    }
 
     dataset_definitions = [profile_dataset_definition, transaction_dataset_definition]
-    relationships = [
-        primary_profile_relationship.output,
-        profile_transaction_relationship.output,
-    ]
+    relationships = [primary_profile_relationship, profile_transaction_relationship]
 
     feature_discovery_settings = [
-        {"name": "enable_days_from_prediction_point", "value": True},
-        {"name": "enable_hour", "value": True},
-        {"name": "enable_categorical_num_unique", "value": False},
-        {"name": "enable_categorical_statistics", "value": False},
-        {"name": "enable_numeric_minimum", "value": True},
-        {"name": "enable_token_counts", "value": False},
-        {"name": "enable_latest_value", "value": True},
-        {"name": "enable_numeric_standard_deviation", "value": True},
-        {"name": "enable_numeric_skewness", "value": False},
-        {"name": "enable_day_of_week", "value": True},
-        {"name": "enable_entropy", "value": False},
-        {"name": "enable_numeric_median", "value": True},
+        {"name": "enable_days_from_prediction_point", "value": False},
         {"name": "enable_word_count", "value": False},
         {"name": "enable_pairwise_time_difference", "value": True},
-        {"name": "enable_days_since_previous_event", "value": True},
-        {"name": "enable_numeric_maximum", "value": True},
-        {"name": "enable_numeric_kurtosis", "value": False},
-        {"name": "enable_most_frequent", "value": False},
-        {"name": "enable_day", "value": True},
-        {"name": "enable_numeric_average", "value": True},
-        {"name": "enable_summarized_counts", "value": False},
-        {"name": "enable_missing_count", "value": True},
-        {"name": "enable_record_count", "value": True},
         {"name": "enable_numeric_sum", "value": True},
     ]
 
@@ -156,13 +121,6 @@ def datarobot_feature_discovery_pipeline(
     (
         create_use_case
         >> [upload_primary_dataset, upload_profile_dataset, upload_transactions_dataset]
-    )
-    cross_downstream(
-        [upload_primary_dataset, upload_profile_dataset, upload_transactions_dataset],
-        [primary_profile_relationship, profile_transaction_relationship],
-    )
-    (
-        [primary_profile_relationship, profile_transaction_relationship]
         >> create_feature_discovery_recipe
         >> create_project
         >> train_models
@@ -170,7 +128,4 @@ def datarobot_feature_discovery_pipeline(
     )
 
 
-datarobot_feature_discovery_pipeline_dag = datarobot_feature_discovery_pipeline()
-
-if __name__ == "__main__":
-    datarobot_feature_discovery_pipeline_dag.test()
+datarobot_feature_discovery_pipeline()
