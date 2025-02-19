8b8f14ab85746e9f5aa43629a71458f5d91eafa6
Christopher Russell-Walker <christopher.russell-walker@datarobot.com>
2025-02-19 10:36:44 -0500

    Fix imports in unit tests

--- tests/unit/_experimental/dags/pipline/test_datarobot_pipeline_dag.py
+++ tests/unit/_experimental/dags/pipline/test_datarobot_pipeline_dag.py
@@ -7,7 +7,9 @@
 # Released under the terms of DataRobot Tool and Utility Agreement.
 import pytest
 
-from datarobot_provider._experimental.example_dags.datarobot_pipeline_dag import datarobot_pipeline
+from datarobot_provider._experimental.deprecated_example_dags.datarobot_pipeline_dag import (
+    datarobot_pipeline,
+)
 
 
 def test_dag_loaded(dagbag):
